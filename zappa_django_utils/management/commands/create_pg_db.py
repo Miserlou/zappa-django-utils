7e87db5da6213a3f96134073de1c942208d16052
Rich Jones <rich@anomos.info>
2017-08-11 15:19:04 -0400

    0.1.0 - fix hardcoded db name bug in db create

--- zappa_django_utils/management/commands/create_pg_db.py
+++ zappa_django_utils/management/commands/create_pg_db.py
@@ -7,20 +7,20 @@ class Command(BaseCommand):
     help = 'Creates the initial Postgres database'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Starting db creation'))
+        self.stdout.write(self.style.SUCCESS('Starting DB creation..'))
 
         dbname = settings.DATABASES['default']['NAME']
         user = settings.DATABASES['default']['USER']
         password = settings.DATABASES['default']['PASSWORD']
         host = settings.DATABASES['default']['HOST']
 
-        con = None
-        con = connect(dbname='postgres', user=user, host = host, password=password)
-        dbname = dbname
+        con = connect(dbname=dbname, user=user, host=host, password=password)
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        
         cur = con.cursor()
         cur.execute('CREATE DATABASE ' + dbname)
         cur.close()
+        
         con.close()
 
-        self.stdout.write(self.style.SUCCESS('All Done'))
+        self.stdout.write(self.style.SUCCESS('All done!'))

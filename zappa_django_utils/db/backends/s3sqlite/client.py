6f16a28b639c54285b7b44a00d7e78596373a691
Allen, Timothy <tallen@wharton.upenn.edu>
2018-01-06 15:15:44 +0000

    Bug fix: subprocess is at the wrong indent level. Also, PEP-8.

--- zappa_django_utils/db/backends/s3sqlite/client.py
+++ zappa_django_utils/db/backends/s3sqlite/client.py
@@ -7,6 +7,9 @@ class DatabaseClient(BaseDatabaseClient):
     executable_name = 'sqlite3'
 
     def runshell(self):
-        args = [self.executable_name,
-                self.connection.settings_dict['NAME']]
-subprocess.check_call(args)
+        args = [
+            self.executable_name,
+            self.connection.settings_dict['NAME'],
+        ]
+
+        subprocess.check_call(args)

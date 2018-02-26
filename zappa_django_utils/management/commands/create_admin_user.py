e5e0e108efaceefe98967694d191fb3ea47d0a6d
Victor Chaptsev <victor@chaptsev.ru>
2018-02-27 02:31:20 +0300

    Feat: create_admin_user arguments
    It's just a bit more flexible solution.

--- zappa_django_utils/management/commands/create_admin_user.py
+++ zappa_django_utils/management/commands/create_admin_user.py
@@ -8,13 +8,26 @@ import string
 class Command(BaseCommand):
     help = 'Creates a default "admin" user'
 
+    def add_arguments(self, parser):
+        parser.add_argument('arguments', nargs='*')
+
     def handle(self, *args, **options):
         # see: https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#referencing-the-user-model
         User = get_user_model()
 
-        pw = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        self.stdout.write(self.style.SUCCESS('Creating new admin user...'))
 
-        self.stdout.write(self.style.SUCCESS('Creating new admin user..'))
-        User.objects.create_superuser('admin', 'admin@admin.com', pw)
-        self.stdout.write(self.style.SUCCESS('Created user "admin", email: "admin@admin.com", password: ' + pw))
-        self.stdout.write(self.style.SUCCESS('Log in and change this password immediately!'))
+        # if the command args are given -> try to create user with given args
+        if options['arguments']:
+            try:
+                User.objects.create_superuser(*options['arguments'])
+                self.stdout.write(self.style.SUCCESS('Created user with given params'))
+            except Exception as e:
+                self.stdout.write(str(e))
+
+        # or create default admin user
+        else:
+            pw = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+            User.objects.create_superuser('admin', 'admin@admin.com', pw)
+            self.stdout.write(self.style.SUCCESS('Created user "admin", email: "admin@admin.com", password: ' + pw))
+            self.stdout.write(self.style.SUCCESS('Log in and change this password immediately!'))
